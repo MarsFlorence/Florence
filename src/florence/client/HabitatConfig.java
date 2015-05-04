e5ea5014446a56008708333c51a7b148a21c1713
Drew <Drew@DrewRensPC>
2015-05-03 21:28:45 -0500

    habitatConfig

--- src/florence/client/HabitatConfig.java
+++ src/florence/client/HabitatConfig.java
@@ -1,5 +1,80 @@
 package florence.client;
 
-public class HabitatConfig {
+import java.util.ArrayList;
 
+public class HabitatConfig {
+	
+	private ModuleLog moduleLog;
+	private Module[][] habitatConfig;
+
+	public ArrayList<Module> plainModules;
+	public ArrayList<Module> dormitoryModules;
+	public ArrayList<Module> sanitationModules;
+	public ArrayList<Module> controlModules;
+	public ArrayList<Module> foodAndWaterModules;
+	public ArrayList<Module> gymAndRelaxationModules;
+	public ArrayList<Module> canteenModules;
+	public ArrayList<Module> powerModules;
+	public ArrayList<Module> airlockModules;
+	public ArrayList<Module> medicalModules;
+	
+	
+	
+	public HabitatConfig(ModuleLog log){
+		moduleLog = log;
+		habitatConfig = new Module[100][100];
+		setTypeArrays();
+		
+	}
+	
+	private void setTypeArrays(){
+		for(Module mod: moduleLog.getModuleLog()){
+			if(mod.getModType() == Type.PLAIN){
+				plainModules.add(mod);
+			}
+			else if(mod.getModType() == Type.AIRLOCK){
+				airlockModules.add(mod);
+			}
+			else if(mod.getModType() == Type.CANTEEN){
+				canteenModules.add(mod);
+			}
+			else if(mod.getModType() == Type.CONTROL){
+				controlModules.add(mod);
+			}
+			else if(mod.getModType() == Type.DORMITORY){
+				dormitoryModules.add(mod);
+			}
+			else if(mod.getModType() == Type.FWS){
+				foodAndWaterModules.add(mod);
+			}
+			else if(mod.getModType() == Type.GR){
+				gymAndRelaxationModules.add(mod);
+			}
+			else if(mod.getModType() == Type.MEDICAL){
+				medicalModules.add(mod);
+			}
+			else if(mod.getModType() == Type.POWER){
+				powerModules.add(mod);
+			}
+			else if(mod.getModType() == Type.SANITATION){
+				sanitationModules.add(mod);
+			}
+		}
+	}
+	public Module[][] getHabitatConfig() {
+		return habitatConfig;
+	}
+
+	public void setHabitatConfig(Module[][] habitatConfig) {
+		this.habitatConfig = habitatConfig;
+	}
+
+	public ModuleLog getModuleLog() {
+		return moduleLog;
+	}
+
+	public void setModuleLog(ModuleLog moduleLog) {
+		this.moduleLog = moduleLog;
+	}
+	
 }
