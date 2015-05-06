8b4e43da6309c7e859c8b8d132fedb422ff81c4c
Jake <Jake@JakeBible-PC>
2015-05-05 20:18:11 -0500

    added first support for saving and loading configurations

--- src/florence/client/HabitatConfig.java
+++ src/florence/client/HabitatConfig.java
@@ -2,6 +2,13 @@ package florence.client;
 
 import java.util.ArrayList;
 
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONNumber;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.storage.client.Storage;
+
 public class HabitatConfig {
 	
 	private ModuleLog moduleLog;
@@ -19,7 +26,9 @@ public class HabitatConfig {
 	private ArrayList<Module> medicalModules;
 	
 	
-	
+	public HabitatConfig(){
+		habitatConfig = new Module[100][100];	
+	}
 	public HabitatConfig(ModuleLog log){
 		moduleLog = log;
 		habitatConfig = new Module[100][100];
@@ -132,6 +141,7 @@ public class HabitatConfig {
 		canteenModules.remove(0);
 		addModule(foodAndWaterModules.get(0), 53, 51);
 		foodAndWaterModules.remove(0);
+		saveConfiguration("min1");
 	}
 	
 	public Module getModuleAtCoordinates(int x, int y){
@@ -176,4 +186,117 @@ public class HabitatConfig {
 		this.moduleLog = moduleLog;
 	}
 	
+	/**
+	 * Saves this habitat configuration onto HTML5 local storage.
+	 * @param name The name of this configuration (ex: "min1").
+	 */
+	public boolean saveConfiguration(final String name) {
+		boolean valid = true;
+		
+		String key = name;
+  		Storage moduleStore = Storage.getLocalStorageIfSupported();	
+  		
+  		
+//  		if (moduleStore != null) {
+//  			String moduleListInfo = "";
+//  			for(Module[] modList : habitatConfig){
+//  				for(Module mod : modList){
+//		  			//In the form: [ID,Status,Orientation,Xcoord,Ycoord],[ID,Status,Orientation,Xcoord,Ycoord],
+//		  			moduleListInfo = moduleListInfo + "[" + mod.getId() + "," + mod.getStatus().toString()
+//		  						+ "," + mod.getOrientation() + "," + mod.getXCoord() + "," + mod.getYCoord() + "],";
+//		  		}	
+//			}
+//  		    // Removes the final ',' placed by the loops.
+//  		    moduleListInfo = moduleListInfo.substring(0, moduleListInfo.length() - 1);
+//  		    moduleListInfo = moduleListInfo + "]";
+//  			moduleStore.setItem(key, moduleListInfo);
+//  		}
+  		
+  		if (moduleStore != null) {
+  			String moduleListInfo = "[";  		
+  			for(int y=0; y < 100; y++){
+  				for(int x=0; x < 100; x++){
+  					if(habitatConfig[x][y] != null){
+  						Module mod = habitatConfig[x][y];
+  						//In the form: {id:"id",status:"status",ori:"ori",xcoord:"xcoord",ycoord:"ycoord",xpos:"xpos",ypos:"ypos"},
+  						moduleListInfo = moduleListInfo + "{id:" + mod.getId() + ",status:" + mod.getStatus().toString()
+		  						+ ",ori:" + mod.getOrientation() + ",xoord:" + mod.getXCoord() + ",ycoord:" + mod.getYCoord() + ",xpos:" + x + ",ypos:" + y + "},";	
+  					}
+  				}
+  			}
+		    // Removes the final ',' placed by the loops.
+		    moduleListInfo = moduleListInfo.substring(0, moduleListInfo.length() - 1);
+		    moduleListInfo = moduleListInfo + "]";
+			moduleStore.setItem(key, moduleListInfo);
+  		}
+		
+		return valid;
+	}
+	/**
+	 * Loads a configuration from local storage into this habitat configuration.
+	 * TODO: NOT TESTED!!!****
+	 * @param name The name of the configuration to load.
+	 * @return true if the configuration was loaded, else false
+	 */
+	public HabitatConfig loadConfiguration(final String name) {
+		Storage store = null;
+		store = Storage.getLocalStorageIfSupported();
+		boolean loaded;
+		
+		String key = name;
+		String value = store.getItem(key);
+		
+		// If the configuration was not found in storage.
+		if (value == null) {
+			loaded = false;
+		} else {
+
+			HabitatConfig habitatConfig = new HabitatConfig();
+			JSONArray array = (JSONArray) JSONParser.parseLenient(value);
+			JSONNumber number;
+			JSONString string;
+			double id;
+			String status;
+			double turns;
+			double x;
+			double y;
+			int xPos;
+			int yPos;
+			for(int i = 0; i < array.size(); i++){
+				JSONObject object = (JSONObject) array.get(i);
+				number = (JSONNumber) object.get("id");
+				id = number.doubleValue();		
+				
+				string = (JSONString) object.get("status");
+				status = string.stringValue();
+				
+				number = (JSONNumber) object.get("ori");
+				turns = number.doubleValue();			
+				
+				number = (JSONNumber) object.get("xoord");
+				x = number.doubleValue();	
+
+				number = (JSONNumber) object.get("ycoord");
+				y = number.doubleValue();	
+
+				number = (JSONNumber) object.get("xpos");
+				xPos = (int) number.doubleValue();	
+
+				number = (JSONNumber) object.get("ypos");
+				yPos = (int) number.doubleValue();
+				
+				Module newModule = new Module();
+				newModule.setId((int) id);
+				newModule.setStatus(status);
+				newModule.setOrientation((int) turns);
+				newModule.setXCoord(x);
+				newModule.setYCoord(y);
+				
+				habitatConfig.getHabitatConfig()[xPos][yPos] = newModule;
+				
+			}
+			return habitatConfig;
+		}
+		return null;
+	}
 }
